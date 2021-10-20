71701b93099825e7ae99ec73bd77f98a44cc1d25
riki-wi <riki-wi@yandex.ru>
2021-10-20 18:22:59 +0300

    add task "translation into the 16 number system"

---
+++ tasks_18_10_2021/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+// helper function translation
+std::string checkNumberForTranslation(int x)
+{
+    switch (x)
+    {
+        case 10: return "A";
+        case 11: return "B";
+        case 12: return "C";
+        case 13: return "D";
+        case 14: return "E";
+        case 15: return "F";
+        default: return std::to_string(x);
+    }
+}
+
+// translation into the 16 number system
+void translation()
+{
+    unsigned long long x = 0;
+    std::string res;
+    std::cout << "Введите целое положительное число: " << std::endl;
+    std::cin >> x;
+    while (x >= 1)
+    {
+        res += checkNumberForTranslation((int)x % 16);
+        x = x >> 4;
+    }
+    std::reverse(res.begin(), res.end());
+    std::cout << res << std::endl;
+}
+
+
+
+int main() {
+
+    translation();
+    return 0;
+}
