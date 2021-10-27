3c78b7734e670e505c101c9f98ce1cc8c1b5b5f1
riki-wi <riki-wi@yandex.ru>
2021-10-27 08:03:36 +0300

    add taskFour (25_10_2021)

--- tasks_25_10_2021/main.cpp
+++ tasks_25_10_2021/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <random>
 
 void initializationMasZero(int* mas, int size)
 {
@@ -11,9 +12,12 @@ void initializationMasZero(int* mas, int size)
 
 void initializationMasRand(int* mas, int size)
 {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> uid(-32767, 32767);
     for (int i = 0; i < size; i++)
     {
-        mas[i] = (rand() - RAND_MAX / 2) % 32767;
+        mas[i] = uid(gen);
     }
 }
 
@@ -117,10 +121,58 @@ void taskThree()
     std::cout << "the most common element " << resIndex << std::endl;
 }
 
+//pairs in ascending order
+void taskFour()
+{
+    const int size = 10;
+    int* mas1 = new int[size];
+    int* mas2 = new int[size];
+    int* res = new int[size * 2];
+    initializationMasRand(mas1, size);
+    initializationMasRand(mas2, size);
+    initializationMasZero(res, size * 2);
+
+    std::cout << "mas first: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << mas1[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "mas second: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << mas2[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0, j = 0; i < size; i++, j += 2)
+    {
+        if(mas1[i] < mas2[i])
+        {
+            res[j] = mas1[i];
+            res[j + 1] = mas2[i];
+        }
+        else
+        {
+            res[j] = mas2[i];
+            res[j + 1] = mas1[i];
+        }
+    }
+
+    std::cout << "mas result: ";
+    for (int i = 0; i < size * 2; i++)
+    {
+        std::cout << res[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
-    taskOne();
+    //taskOne();
     //taskTwo();
     //taskThree();
+    taskFour();
     return 0;
 }
