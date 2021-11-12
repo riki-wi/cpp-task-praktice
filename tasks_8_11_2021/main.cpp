3f76b2db3d308308460816b692b5ae4a8cb0d6b3
riki-wi <riki-wi@yandex.ru>
2021-11-12 14:16:26 +0300

    add snakeCenter task

--- tasks_8_11_2021/main.cpp
+++ tasks_8_11_2021/main.cpp
@@ -2,17 +2,17 @@
 
 int** creatIndependentTwoDimensionalMas(int n, int m)
 {
-    int** mas = new int*[m];
+    int** mas = new int*[n];
     for(int i = 0; i < n; i++)
     {
-        mas[i] = new int[m];
+        mas[i] = new int[m]{0};
     }
     return mas;
 }
 
 int** createDependentTwoDimensionalMas(int n, int m)
 {
-    int* mas = new int[n * m];
+    int* mas = new int[n * m]{0};
     int** masAdd = new int*[n];
     for(int i = 0, j = 0; i < n * m; i += m, j++)
     {
@@ -69,6 +69,39 @@ void snakeDiagonal(int** mas, int n, int m)
 }
 
 
+//filling an array with diagonal a snake
+void snakeCenter(int** mas, int n, int m)
+{
+    int date = 0;
+    for(int i = 0, mJ = 0; i < m / 2; mJ++, i++)
+    {
+        for(int j = i; j < m - i; j++)
+        {
+            mas[i][j] = date;
+            date++;
+        }
+        for(int j = mJ + 1; j < n - mJ; j++)
+        {
+            mas[j][m - mJ - 1] = date;
+            date++;
+        }
+        for(int j = m - mJ - 2; j > mJ; j--)
+        {
+            mas[n - mJ - 1][j] = date;
+            date++;
+        }
+        for(int j = n - 1 - mJ; j > mJ; j--)
+        {
+            mas[j][mJ] = date;
+            date++;
+        }
+    }
+    if((n == m) && (n % 2))
+        mas[n / 2][m / 2] = date;
+    printDimensionalMas(mas, n, m);
+}
+
+
 int main()
 {
     int n;
@@ -77,10 +110,14 @@ int main()
     std::cin >> n;
     std::cout << "Enter number of columns: ";
     std::cin >> m;
+
     snake(creatIndependentTwoDimensionalMas(n, m), n, m);
+
     std::cout << std::endl;
     std::cout << std::endl;
     snakeDiagonal(createDependentTwoDimensionalMas(n, m), n, m);
 
-
+    std::cout << std::endl;
+    std::cout << std::endl;
+    snakeCenter(createDependentTwoDimensionalMas(n, m), n, m);
 }
