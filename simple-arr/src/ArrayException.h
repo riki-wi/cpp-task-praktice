c8cda40a6f13175a83de6b6057bc3e3095ebc02f
riki-wi <riki-wi@yandex.ru>
2022-03-15 19:24:31 +0300

    добавлен simple-arr

---
+++ simple-arr/src/ArrayException.h
@@ -0,0 +1,29 @@
+#ifndef SIMPLE_ARR_ARRAYEXCEPTION_H
+#define SIMPLE_ARR_ARRAYEXCEPTION_H
+
+#include "iostream"
+
+class IndexException: public std::exception
+{
+
+public:
+    [[nodiscard]] const char * what() const noexcept override
+    {
+        return "Bad Index!";
+    }
+
+};
+
+class SizeException: public std::exception
+{
+
+public:
+    [[nodiscard]] const char * what() const noexcept override
+    {
+        return "Bad Size!";
+    }
+};
+
+
+
+#endif
