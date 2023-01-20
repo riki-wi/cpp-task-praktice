3035bc235f14e74a138113dc8653eae543e5cea4
Andrey Shemyakin <76729631+riki-wi@users.noreply.github.com>
2023-01-21 02:56:03 +0300

    паралельное вычисление числа пи

--- 2-semestr/task-async/main.cpp
+++ 2-semestr/task-async/main.cpp
@@ -2,14 +2,34 @@
 #include <future>
 #include <cmath>
 
-double pi(int n)
+const int N = 1000000;
+const int n = N+1;
+double PI = 0;
+
+int block_size(int threads)
 {
-    double pi;
-    for (int i = 0; i < n; i++)
+    return n / threads + (n % threads ? 1 : 0);
+}
+
+void pi_block(int threads, int first_index)
+{
+    int bl_size = block_size(threads);
+    for(int i = first_index; i < std::min(first_index + bl_size, n); i++)
     {
-        pi = pi + std::sqrt(12) * std::pow(-1, i) / ((2.0 * i + 1) * (std::pow(3, i)));
+        PI += std::sqrt(12) * std::pow(-1, i) / ((2.0 * i + 1) * std::pow(3, i));
     }
-    return pi;
+}
+
+void pi(int threads)
+{
+    int bl_size = block_size(threads);
+    int first_index = 0;
+    while(first_index < n)
+    {
+        std::future<void> f1 = std::async(pi_block, threads, first_index);
+        first_index += bl_size;
+        f1.get();
+    }
 }
 
 //------------------------------------------------------------------------------------------------------
@@ -17,10 +37,10 @@ double pi(int n)
 
 int main()
 {
-    std::future<double> f1 = std::async(pi, 100);
+    pi(100);
     std::cout << std::fixed;
     std::cout.precision(16);
-    std::cout << "pi = " << f1.get() << std::endl;
+    std::cout << "pi = " << PI << std::endl;
 
     //---------------------------------------------
 
